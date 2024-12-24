476b66da8e6cc85f57abd600df1e797129a3c78c
zoolotoui <chag.med.amine@gmail.com>
2024-12-24 13:51:28 +0100

    init: starting up project

---
+++ src/app/components/game/game.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-game',
+  imports: [],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.css'
+})
+export class GameComponent {
+
+}
