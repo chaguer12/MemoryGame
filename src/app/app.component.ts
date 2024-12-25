a4b018f6c7eac31082d1d2f3852939f14b2c9e0d
zoolotoui <chag.med.amine@gmail.com>
2024-12-25 09:43:33 +0100

    stat: storing player name & working on a transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   userName: string  = '';
+  startGame():void {
+    if(this.userName){
+      console.log(`starting game for ${this.userName}`)
+    }else{
+      console.log("please enter your username!")
+    }
+  }
+
+
 }
