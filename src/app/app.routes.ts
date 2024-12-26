ac78b9d9e666d70d0aca23affa9a25a72f4bb6e1
zoolotoui <chag.med.amine@gmail.com>
2024-12-26 11:32:58 +0100

    stat: fixing the routing problem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
 import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
 
 export const routes: Routes = [
-
+  {path:'',component:HomeComponent},
   {path:'game',component:GameComponent}
 ];
